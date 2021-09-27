a7a6e0653d3555bf54f1662b6dab49f233b53561
Birk Wertwein <birk@probisma.se>
2021-09-27 22:04:28 +0200

    * add inboundsOnly prop to entity * pass position to entity constructor

--- src/entities/Entity.js
+++ src/entities/Entity.js
@@ -1,37 +1,42 @@
 export class Entity {
+  constructor(position, inBoundsOnly = false) {
+    this.components = [];
+    this.position = position;
+    this.inBoundsOnly = inBoundsOnly;
+  }
 
-    constructor() {
-        this.components = [];
-        this.position = {x: 0, y: 0};
+  getComponent(type) {
+    for (const component of this.components) {
+      if (component instanceof type) {
+        return component;
+      }
     }
+  }
 
-    getComponent(type) {
-        for (const component of this.components) {
-            if (component instanceof type) {
-                return component
-            }
-        }
+  addComponents(...components) {
+    for (const component of components) {
+      this.components.push(component);
     }
+  }
 
-    addComponents(...components) {
-        for (const component of components) {
-            this.components.push(component);
-        }
+  hasComponent(type) {
+    for (const component of this.components) {
+      if (component instanceof type) {
+        return true;
+      }
     }
+  }
 
-    hasComponent(type) {
-        for (const component of this.components) {
-            if (component instanceof type) {
-                return true
-            }
-        }
-    }
+  removeComponent(type) {
+    this.components = this.components.filter(
+      (component) => component instanceof type
+    );
+  }
 
-    draw(ctx) {
-        ctx.fillStyle = "#00f";
-        ctx.beginPath();
-        ctx.arc(this.position.x, this.position.y, 16, 0, 2 * Math.PI);
-        ctx.fill();
-    }
-
+  draw(ctx) {
+    ctx.fillStyle = "#00f";
+    ctx.beginPath();
+    ctx.arc(this.position.x, this.position.y, 16, 0, 2 * Math.PI);
+    ctx.fill();
+  }
 }
