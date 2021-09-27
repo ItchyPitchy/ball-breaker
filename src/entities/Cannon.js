6ca07017452ce12f61bc26fa4d606c1fe3d29856
Birk Wertwein <birk@probisma.se>
2021-09-27 22:14:55 +0200

    change how cannon should rotate

--- src/entities/Cannon.js
+++ src/entities/Cannon.js
@@ -1,25 +1,22 @@
 import { Entity } from "./Entity.js";
 
 export class Cannon extends Entity {
+  constructor(position) {
+    super(position);
+    this.degrees;
+  }
 
-    constructor(position) {
-        super();
-        this.position = position;
-        this.degrees;
-    }
+  draw(ctx) {
+    // Matrix transformation
+    ctx.save();
+    ctx.translate(this.position.x, this.position.y);
+    ctx.rotate(this.degrees);
+    ctx.translate(-30, -(20 / 2));
 
-    draw(ctx) {
+    // Rotated rectangle
+    ctx.fillStyle = "red";
+    ctx.fillRect(50, 0, 30, 20);
 
-        // Matrix transformation
-        ctx.translate(this.position.x, this.position.y);
-        ctx.rotate(this.degrees);
-        ctx.translate(-(this.position.x + 140 / 2), -(this.position.y + 75 / 2));
-
-        // Rotated rectangle
-        ctx.fillStyle = 'red';
-        ctx.fillRect(this.position.x, this.position.y, 140, 75);
-
-        ctx.setTransform(1, 0, 0, 1, 0, 0);
-    }
-
+    ctx.restore();
+  }
 }
