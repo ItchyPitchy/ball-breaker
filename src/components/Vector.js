ce5f90dad013db6dfaf6e7b60e64872c5e27fe0e
Birk Wertwein <birk@probisma.se>
2021-10-04 01:14:16 +0200

    add methods

--- src/components/Vector.js
+++ src/components/Vector.js
@@ -4,6 +4,27 @@ export class Vector {
     this.y = y;
   }
 
+  magnitude() {
+    return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+  }
+
+  dot(vector) {
+    return this.x * vector.x + this.y * vector.y;
+  }
+
+  norm() {
+    return {
+      x: this.x / this.magnitude(),
+      y: this.y / this.magnitude(),
+    };
+  }
+
+  normalize() {
+    const norm = this.norm();
+
+    return new Vector(norm.x, norm.y);
+  }
+
   clone() {
     return new Vector(this.x, this.y);
   }
